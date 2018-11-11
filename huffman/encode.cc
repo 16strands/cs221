7386243b19c414ae0a30d2e4b98e8c41ea009ba6
Sarah Strand <39971563+16strands@users.noreply.github.com>
2018-11-11 11:41:20 -0800

    Sarah's stuff

--- huffman/encode.cc
+++ huffman/encode.cc
@@ -1,7 +1,35 @@
+#include <fstream>
+
 #include "huffman.hh"
 #include "bitio.hh"
 
-int main(){
-  // read a file or something
-  // loop through building forests and calling bitio for each char
+int main(int argc, char *argv[]){
+    for ( int i = 0; i< argc ; i++){
+        std::ifstream infile {argv[i]};
+        std::string infile_name = argv[i];
+        assert(infile);
+        std::string outfile_name = infile_name + ".comp";
+        std::ofstream outfile{outfile_name};
+        assert(outfile);
+        int symbol;
+        Huffman huffman;
+        BitIO bitio(std::istream is);
+        while (symbol != Huffman::HEOF){
+            infile >> symbol;
+            Huffman::bits_t bools_list = huffman.Huffman::encode(symbol);
+            for (bool bool_t : bools_list){
+                bitio.BitIO::output_bit(bool_t);
+            }
+        Huffman::bits_t EOF_asboollist = huffman.Huffman::encode(Huffman::HEOF);
+        for (bool bool_heof : EOF_asboollist){
+            bitio.BitIO::output_bit(bool_heof);
+
+        }
+    }
+    infile.close();
+    outfile.close();
+    }
+return 0;
 }
+
+
--- huffman/huffman.cc
+++ huffman/huffman.cc
@@ -1,31 +1,101 @@
-/*
- * Huffman: a class to encode and decode symbols based on adaptive Huffman coding
- * https://en.wikipedia.org/wiki/Huffman_coding
- */
-
 #include <climits>
 #include <vector>
+
+#include "htree.hh"
+#include "hforest.hh"
 #include "huffman.hh"
-//#include "htree.hh"
 
-// Maximum number of symbols we need to encode (a symbol fits in a char) + EOF
-static const int ALPHABET_SIZE = (1 << CHAR_BIT) + 1;  // (2 ** 8) + 1
-static const int HEOF = ALPHABET_SIZE - 1;  // Special symbol to denote end of file
+  using bits_t = std::vector<bool>;
 
-using bits_t = std::vector<bool>;
+  Huffman::Huffman(){
+    freqs = freqs;
+    for ( int i=0; i<257; i++){
+      freqs.vector::push_back(0);
+    }
+    forest = Huffman::forest;
+    huff_tree = Huffman::huff_tree;
+    decode_node = Huffman::decode_node;
 
-Huffman::Huffman(){
-  
-}
+  }
 
-// Encode a symbol into a sequence of bits, then update frequency table.
-bits_t encode(int symbol){
-}
+  void Huffman::build_forest(){
+    for (int i=0; i<257; i++){
+      forest.add_tree(HTree::tree_ptr_t(new HTree(i, freqs.vector::at(i))));
+    }
+  }
+  void Huffman::build_huff_tree(){
+    while (forest.HForest::size() > 1){
+      HTree::tree_ptr_t t1 = forest.HForest::pop_top();
+      HTree::tree_ptr_t t2 = forest.HForest::pop_top();
+      HForest::tree_t t3 = HForest::tree_t(new HTree::HTree(-1, (t1->get_value()+t2->get_value())));
+      forest.HForest::add_tree(t3);
+      huff_tree = t3;
+    }
+  }
 
-// Decode a single bit into a symbol. If no symbol can be unmabiguously decoded
-// without additional bits, returns a negative value.
-// Subsequent calls with more bits should eventually resolve to either
-// a character symbol or HEOF.
-// Finally, updates the frequency table with this additional symbol.
-int decode(bool bit){
-}
+  bits_t Huffman::path_to_bools(HTree::path_t path){
+    bits_t path_bits;
+    for ( HTree::Direction x : path){
+      if (x == HTree::Direction::LEFT){
+        path_bits.push_back(false);
+      }
+      else{
+        path_bits.push_back(true);
+      }
+    }
+    return path_bits;
+  }
+
+
+
+
+
+  // Encode a symbol into a sequence of bits, then update frequency table.
+  bits_t Huffman::encode(int symbol){
+    build_forest();
+    build_huff_tree();
+    HTree::path_t path = huff_tree->path_to(symbol);
+    bits_t path_bits = path_to_bools(path);
+    freqs.vector::at(symbol) += 1;
+    return path_bits;
+  }
+
+  // Decode a single bit into a symbol. If no symbol can be unmabiguously decoded
+  // without additional bits, returns a negative value.
+  // Subsequent calls with more bits should eventually resolve to either
+  // a character symbol or HEOF.
+  // Finally, updates the frequency table with this additional symbol.
+  int Huffman::decode(bool bit){
+    HForest::tree_t next_t;
+    if (!decode_node){
+      build_forest();
+      build_huff_tree();
+      decode_node = huff_tree;
+    }
+      if (bit == false){
+        next_t = decode_node->get_child(HTree::Direction::LEFT);
+      }
+      else if (bit == true){
+        next_t = decode_node->get_child(HTree::Direction::RIGHT);
+      }
+      if (next_t->get_key() == -1){
+        decode_node = next_t;
+        return -1;
+      }
+      else{
+        freqs.vector::at(next_t->get_key()) += 1;
+        build_forest();
+        build_huff_tree();
+        decode_node = huff_tree;
+        return next_t->get_key();
+      }
+    }
+
+
+
+
+
+
+
+
+
