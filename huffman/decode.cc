9dbdba06a13a40ae533f8324bb8bd98ff583ee59
Sarah Strand <39971563+16strands@users.noreply.github.com>
2018-11-11 11:50:09 -0800

    Sarah's decode.cc

--- huffman/decode.cc
+++ huffman/decode.cc
@@ -1,7 +1,32 @@
-#include "huffman.hh"
-#include "bitio.hh"
+#include <fstream>
 
-int main(){
-  // bitio stuff
-  // build a tree
+#include "bitio.hh"
+#include "huffman.hh"
+
+int main(int argc, char *argv[]){
+    for ( int i = 0; i< argc ; i++){
+        std::ifstream infile {argv[i]};
+        std::string infile_name = argv[i];
+        assert(infile);
+        std::string outfile_name = infile_name + ".plaintext";
+        std::ofstream outfile{outfile_name};
+        assert(outfile);
+        Huffman huffman;
+        BitIO bitio(std::ofstream ofs_);
+        while (true){
+            int symbol = -1;
+            while (symbol == -1){
+                bool bit = bitio.input_bit();
+                symbol = huffman.decode(bit);
+            
+            }
+            outfile << symbol;
+            if (symbol == Huffman::HEOF){
+                break;
+            }
+    }
+    infile.close();
+    outfile.close();
+    }
+return 0;
 }
